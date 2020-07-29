36b0f2e5c53c0fe7b868f1158508e1600f25c12c
bsrcs <bsr.ercelik@gmail.com>
2020-07-29 13:31:06 +0300

    React Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React, {useState} from "react";
 
 function App() {
 
-  const [headingText, setHeadingText] = useState("Hello!");
+  const [headingText, setHeadingText] = useState("");
   const [isMouseOver, setMouseOver] = useState(false); 
+  const [name, setName] = useState("");
 
-  function handleClick(){
-    setHeadingText("Submitted");
+  function handleClick(event){
+    setHeadingText(name);
+    event.preventDefault();
   }
 
   function handleMouseOver(){
@@ -17,14 +19,32 @@ function App() {
     setMouseOver(false);
   }
 
+  // "event" objekti onChange'i trigger eder.
+  // "target" the element that triggered this "event".
+  // value corresponds to value of attributes.
+  function handleChange(event){
+    //console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+// input elementi ne zaman "onChange"de store edilen "handleChange" fonk.ununu trigger ediyor?
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button style = {{backgroundColor: isMouseOver? "black": "white"}}
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+      <input 
+      onChange={handleChange}
+      type="text" 
+      placeholder="What's your name?"
+      value={name}
+       />
+      <button 
+      style = {{backgroundColor: isMouseOver? "black": "white"}}
       onMouseOver={handleMouseOver} 
       onMouseOut ={handleMouseOut}
-      onClick={handleClick}>Submit</button>
+      >Submit</button>
+      </form>
     </div>
   );
 }
