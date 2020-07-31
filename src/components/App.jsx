c957e41f496b3583da64fd0698598372f496014f
bsrcs <bsr.ercelik@gmail.com>
2020-07-31 18:46:10 +0300

    use spread operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 
 function App() {
 
- const [isMouseOver, setMouseOver] = useState(false);
- // instead of storing simple value we're storing an object
-  const [fullName, setFullName] = useState({
+  const [isMouseOver, setMouseOver] = useState(false);
+  const [contact, setContact] = useState({
     fname: "",
-    lname: ""
+    lname: "",
+    email: ""
   });
 
   function handleMouseOver() {
@@ -17,51 +17,30 @@ function App() {
     setMouseOver(false);
   }
 
-  // return fname & lname as an object to see both on heading
-  function handleChange(event){
-    // const newValue = event.target.value;
-    // const inputName = event.target.name;
-    
-    // instead we can use object destructuring
-    const {value, name} = event.object;
+  function handleChange(event) {
 
-    setFullName(prevValue => {
-      if(name === "fname"){
-        return{
-          fname: value,
-          lname: prevValue.lname
-        };
-      }
-      else if(name === "lname"){
-        return{
-          fname: prevValue.fname,
-          lname: value
-        };
+    const { value, name } = event.target;
+
+    setContact(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value
       }
     });
   }
 
   return (
     <div className="container">
-      <h1>Hello {fullName.fname} {fullName.lname}</h1>
+      <h1>Hello {contact.fName} {contact.lName}</h1>
       <form>
-        <input
-          onChange={handleChange}
-          name="fname"
-          placeholder="First Name"
-          value={fullName.fname}
-        />
-        <input
-          onChange={handleChange}
-          name="lname"
-          placeholder="Last Name"
-          value={fullName.lname}
-        />
+        <input onChange={handleChange} name="fName" value={contact.fName} placeholder="First Name" />
+        <input onChange={handleChange} name="lName" value={contact.lName} placeholder="Last Name" />
+        <input onChange={handleChange} name="email" value={contact.email} placeholder="Email" />
         <button
           style={{ backgroundColor: isMouseOver ? "black" : "white" }}
           onMouseOver={handleMouseOver}
           onMouseOut={handleMouseOut}
-          >Submit</button>
+        >Submit</button>
       </form>
     </div>
   );
